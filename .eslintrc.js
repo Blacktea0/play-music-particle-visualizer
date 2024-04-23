3d101a6a4b6ffeaef81aabfa6ee2dd4f22a0be12
Black_tea <13018564+Blacktea0@users.noreply.github.com>
2024-04-23 22:05:10 +0800

    add jest.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,12 @@ module.exports = {
     {
       files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
       extends: 'love'
+    },
+    {
+      files: ['test/**/*'],
+      env: {
+        jest: true
+      }
     }
   ]
 }
