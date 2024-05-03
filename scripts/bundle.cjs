e557c2727f8789342b3247c36ec3f93d78628375
Black_tea <13018564+Blacktea0@users.noreply.github.com>
2024-05-03 19:13:30 +0800

    update build script.

--- scripts/bundle.cjs
+++ scripts/bundle.cjs
@@ -3,18 +3,22 @@ const { configure } = require('esbd')
 const glslxPlugin = require('./libs/esbuild-plugin-glslx')
 const { livereloadPlugin } = require('@jgoz/esbuild-plugin-livereload')
 
+let plugins = [
+  glslxPlugin({
+    writeTypeDeclarations: true,
+    renaming: 'all'
+  }),
+]
+if (process.argv[1] === 'serve') {
+  plugins += livereloadPlugin({})
+}
+
 configure({
   entryPoints: ['./index.html'],
   sourcemap: true,
   outdir: './dist',
   absWorkingDir: __dirname + '/..',
-  plugins: [
-    glslxPlugin({
-      writeTypeDeclarations: true,
-      renaming: 'all'
-    }),
-    livereloadPlugin({})
-  ],
+  plugins: plugins,
   loader: {
     '.mp3': 'file'
   }
